682d8924b77e2999b8b7f124537071d8066af4b7
elPablo <elpablo.volgograd@yandex.ru>
2021-12-24 12:30:08 +0300

    room add view

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import com.example.notes.view.NoteDetailFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+private const val transaction = "TRANSACTION"
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, NoteDetailFragment())
+                .addToBackStack(transaction)
                 .commit()
         }
     }
